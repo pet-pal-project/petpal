17abc014ed90005d8d3102aead5585e46815279e
bhagh <haghb1@gmail.com>
2019-07-30 10:31:10 -0400

    merge

--- petpal/settings.py
+++ petpal/settings.py
@@ -80,8 +80,13 @@ WSGI_APPLICATION = 'petpal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'petpal_db',
+        'USER': 'admin',
+        'PASSWORD': 'momentum2019',
+        'HOST': 'localhost',
+        'PORT': 5432,
+
     }
 }
 
