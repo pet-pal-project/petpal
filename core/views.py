968171839bbf9394208d19c03a30ceda6ad7f138
Amanda Phillipson <amanda.minton@gmail.com>
2019-07-31 18:06:35 -0400

    updated some form information, and started email capabilities

--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from core.models import Pet, Routine, Critical, Profile
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from django.core.mail import send_mail
 
 
 # Create your views here.
@@ -38,3 +39,52 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'registration/registration_form.html', {'form': form})
+
+
+
+def sitter_arrived(request):
+    send_mail(
+        'Your sitter has arrived.',
+        'Hi { owner }, we are notifiying you that your sitter, { sitter } , has successfully checked in for their visit today.',
+        'admin@pet-pal.com',
+        ['amanda.minton@gmail.com'],
+        fail_silently=False,
+    )
+
+    return HttpResponse('Mail successfully sent')
+
+
+def sitter_departed(request):
+    send_mail(
+        'Visit for today is complete.',
+        'Hi { owner }, we are notifiying you that your sitter, { sitter } , has successfully checked out from their visit today. To reveiew their completed tasks, click here',
+        'admin@pet-pal.com',
+        ['amanda.minton@gmail.com'],
+        fail_silently=False,
+    )
+
+    return HttpResponse('Mail successfully sent')
+
+
+def critical_task_missed(request):
+    send_mail(
+        'Critical task for today NOT marked complete.',
+        'Hi { owner }, we are notifiying you that our system does not yet have a record of {the critical task} being marked complete for the visit today. To reveiew tasks, please click here',
+        'admin@pet-pal.com',
+        ['amanda.minton@gmail.com'],
+        fail_silently=False,
+    )
+
+    return HttpResponse('Mail successfully sent')
+
+
+def critical_task_complete(request):
+    send_mail(
+        'Your sitter has completed a critical task for today.',
+        'Hi { owner }, we are notifiying you that our system shows {sitter} has marked {the critical task} complete for the visit today. To reveiew tasks, please click here',
+        'admin@pet-pal.com',
+        ['amanda.minton@gmail.com'],
+        fail_silently=False,
+    )
+
+    return HttpResponse('Mail successfully sent')
