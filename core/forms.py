61f85ffa3e5788887fc98d2ceaacc5bcc99caad0
katelucas <lucas.katie@gmail.com>
2019-08-04 18:06:26 -0400

    update calendar dropdown

--- core/forms.py
+++ core/forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from core.models import Pet, Profile
 from django.forms import ModelForm
 import datetime
+from functools import partial
 
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
@@ -32,10 +33,12 @@ class ProfileForm(ModelForm):
   
 
 class ChecklistForm(forms.Form):
-   sitter = forms.CharField(max_length=50)
-   date = forms.DateField(initial=datetime.date.today)
-   task1 = forms.CharField(max_length=300)
-   task2 = forms.CharField(max_length=300)
-   task3 = forms.CharField(max_length=300)
-   task4 = forms.CharField(max_length=300)
-   task5 = forms.CharField(max_length=300)
+    DateInput = partial(forms.DateInput, {'class': 'datepicker'})
+    sitter = forms.CharField(max_length=50)
+    start_date = forms.DateField(widget=DateInput())
+    end_date = forms.DateField(widget=DateInput())
+    task1 = forms.CharField(max_length=300)
+    task2 = forms.CharField(max_length=300)
+    task3 = forms.CharField(max_length=300)
+    task4 = forms.CharField(max_length=300)
+    task5 = forms.CharField(max_length=300)
