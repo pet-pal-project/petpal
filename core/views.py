94cf09a566201a668a0fb3812abe3a73d087f3fa
bhagh <haghb1@gmail.com>
2019-08-05 13:35:28 -0400

    views update

--- core/views.py
+++ core/views.py
@@ -158,16 +158,14 @@ def critical_task_complete(request):
 
 @login_required
 def update_profile(request):
-    
-        current_user = request.user
-        user = Profile.objects.get(user=current_user)
+            user = Profile.objects.get(user=request.user)
   
         if request.method == "POST":
-            form = ProfileForm(request.POST,instance=user)
+            form = ProfileForm(request.POST,instance=request.user)
             if form.is_valid():
                 profile = form.save(commit=False)
                 profile.save()
             return redirect(to='home')
         else:
-            form = ProfileForm()
+            form = ProfileForm(instance=request.user)
         return render(request, 'update_profile.html', {'form': form})
